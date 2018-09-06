c6180c8ff2c5e4dc6b8d1190bc7529d2a86d6f15
lpuentes19 <luey.puentes@gmail.com>
2018-09-06 11:59:39 -0600

    Completed mapViewController and CreateNewPlaceVC

--- Places2/Places2/View Controllers/MapViewController.swift
+++ Places2/Places2/View Controllers/MapViewController.swift
@@ -7,29 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Properties
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var location: (latitude: Double, longitude: Double)? {
+        didSet {
+            updateMapViewLocation()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateMapViewLocation()
     }
-    */
 
+    private func updateMapViewLocation() {
+        guard let location = location, isViewLoaded else { return }
+        
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
 }
