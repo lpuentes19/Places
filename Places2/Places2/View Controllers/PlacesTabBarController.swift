205b0dcb4a2998754a7e2347157e24e7d30c0b4b
lpuentes19 <luey.puentes@gmail.com>
2018-09-06 11:18:04 -0600

    Subclassed views in storyboard with the appropriate files

---
+++ Places2/Places2/View Controllers/PlacesTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  PlacesTabBarController.swift
+//  Places2
+//
+//  Created by Luis Puentes on 9/6/18.
+//  Copyright © 2018 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class PlacesTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
