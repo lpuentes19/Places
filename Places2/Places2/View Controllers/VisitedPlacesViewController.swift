432d24f7773c3f1a8bf8bcaaeefa831d2d12c32a
lpuentes19 <luey.puentes@gmail.com>
2018-09-06 13:38:32 -0600

    Got the places names to show on the tableviewController

--- Places2/Places2/View Controllers/VisitedPlacesViewController.swift
+++ Places2/Places2/View Controllers/VisitedPlacesViewController.swift
@@ -11,21 +11,28 @@ import UIKit
 class VisitedPlacesViewController: UIViewController, PlacesPresenter {
 
     var placeController: PlaceController?
+    var placesTableViewController: PlacesTableViewController?
+    var mapViewController: MapViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "PlacesTable" {
+            
+            guard let placesTVC = segue.destination as? PlacesTableViewController else { return }
+            
+            placesTVC.placeController = placeController
+            placesTableViewController = placesTVC
+            
+        } else if segue.identifier == "MapView" {
+            
+            guard let mapVC = segue.destination as? MapViewController else { return }
+            
+            mapViewController = mapVC
+        }
     }
-    */
-
 }
